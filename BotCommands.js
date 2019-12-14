e9689382caf4ddc77f505bf1e0ab1d08993ab828
Roel04 <58885330+Roel04@users.noreply.github.com>
2019-12-14 18:55:47 +0100

    Update BotCommands.js

--- BotCommands.js
+++ BotCommands.js
@@ -8,18 +8,10 @@ class FunctionObject {
   ShowShortcuts	( NO_ARGUMENTS ){ 	ShowShortcuts( ); 				}
   Open			( sLink ){ 			Open( sLink ); 					}
   Google		( sKeywords ){ 		Google( sKeywords ); 			}
+  SpamMessage	( sMsg_and_iIt ){	SpamMessage( sMsg_and_iIt );	}
   
 }
 
-/*var FunctionObject = {
-  
-  ShowHelpMenu: 	function( NO_ARGUMENTS ){ 	ShowHelpMenu( ); 					}
-  ChangePrefix: 	function( sNewPrefix ){ 	ChangePrefix( sNewPrefix ); 		}
-  ShowShortcuts: 	function( NO_ARGUMENTS ){ 	ShowShortcuts( ); 					}
-  Open: 			function( sLink ){			Open( sLink );						}
-  Google:			function( sKeywords ){		Google( sKeywords );				}
-  
-};*/
 
 /////////////////////////   Help function   /////////////////////////
 
@@ -28,8 +20,7 @@ let aFunctions =
   [ "[p]help" , "Show this menu" , [ "[p]h" ] ],
   [ "[p]prefix [new prefix]" , "Change the prefix", [ "[p]pre" , "[p]p" ] ],
   [ "[p]shortcuts" , "Show the shortcut of every command" , [ "[p]short" ] ],
-  [ "[p]open [link]" , "Open a link" , [ "No shortcuts" ] , "Open" ],
-  [ "[p]google [keyword(s)]" , "Search google with your keywords" , [ "[p]g" ] ],
+  [ "[p]spam [message] [amount of times]" , "Spam a message an x amount of times" , [ "[p]s" ] ]
   
 ];
 
@@ -38,8 +29,7 @@ let aCallFuncs =
   [ "help" , "h" , "ShowHelpMenu" ],
   [ "prefix" , "pre" , "p" , "ChangePrefix" ],
   [ "shortcuts" , "short" , "ShowShortcuts" ],
-  [ "open" , "Open" ],
-  [ "google" , "g" , "Google" ]
+  [ "spam" , "s" , "SpamMessage" ]
 ];
 
 function ShowHelpMenu( NO_ARGUMENTS ){
@@ -58,12 +48,12 @@ function ShowHelpMenu( NO_ARGUMENTS ){
 /////////////////////////   Prefix Stuff   /////////////////////////
 
 let sPrefix = "!";
-
+ 
 function ChangePrefix( sNewPrefix ){
   
-  Send( `The prefix has changed from "${ sPrefix }" to "${ sNewPrefix }"` );
+  Send( `The prefix has changed from "${ sPrefix }" to "${ sNewPrefix.split(' ').join('').toLowerCase() }"` );
   
-  sPrefix = sNewPrefix;
+  sPrefix = sNewPrefix.split(' ').join('').toLowerCase();
   
 }
 
@@ -84,24 +74,138 @@ function ShowShortcuts( NO_ARGUMENTS ){
 
 
 
-/////////////////////////   Open command   /////////////////////////
+/////////////////////////   Spam function  /////////////////////////
 
-function Open( sLink ){
+function SpamMessage( sMessage_and_iIterations ){
   
-  Send( `Opening link: ${ sLink }` );
+  let sMessage = sMessage_and_iIterations.slice( 0 , sMessage_and_iIterations.indexOf( ' ' ) );
   
-  window.open( sLink , "_blank" , [ width=window.outerWidth, height=window.outerHeight ] );
+  let iIterations = sMessage_and_iIterations.slice( sMessage_and_iIterations.indexOf( ' ' ) + 1 );
   
-}  
+  for( let i = 0; i < iIterations; i++ )
+	Send( sMessage );
+
+}
 
 
 
-/////////////////////////   Search command   /////////////////////////
+/////////////////////////   Process the new message   /////////////////////////
 
-function Google( sKeywords ){
+function ProcessMessage( sMessage ){
   
-  Send( `Searching "${ sKeywords }" on Google.` );
+  let bIsCommand = true;
   
-  window.open( `https://www.google.com/search?&q=${ sKeywords }` , "_blank" , [ width=window.outerWidth, height=window.outerHeight ] );
+  //check if the prefix is correct / if it is a command
+  for( let i = 0; i < sPrefix.length; i ++ )
+	if( sMessage[i] != sPrefix[i] ) 
+	  bIsCommand = false;
+  
+  if( bIsCommand && sMessage.length > sPrefix.length ){
+	
+	//string without prefix and arguments (command)
+    let sCommand;
+    
+    //string without prefix and command (arguments)
+    let sArgument;
+	
+	if( sMessage.indexOf(' ') === -1 || sMessage.indexOf(' ') === sMessage.length - 1 ){
+	  
+	  sCommand = sMessage.slice( sPrefix.length ).toLowerCase();
+	  
+	  sArgument = '';
+	
+	} else {
+	  
+	  sCommand = sMessage.slice( sPrefix.length, sMessage.indexOf( ' ' ) ).toLowerCase();
+	  
+	  sArgument = sMessage.slice( sMessage.indexOf( ' ' ) + 1 );
+	  
+	}
+    
+    //search in the functions list for the entered command and 
+    //return the function version of it (i.e. s -> Search & pre -> ChangePrefix)
+    for( let aI of aCallFuncs )
+      for( let sJ of aI )
+        if( sCommand === sJ )
+          sCommand = aI[ aI.length - 1 ];
+          
+    //call the command's function
+    let callFunction = new FunctionObject();
+	
+	callFunction[ sCommand ]( sArgument );
+	
+  }
   
 }
+
+
+
+/////////////////////////   Send a message   /////////////////////////
+
+function Send( input ) {
+	
+  var evt = new Event( 'input', { bubbles:true, composer:true } );
+  
+  document.querySelector( "#main > footer > div > div > div > div.copyable-text" ).innerHTML = input;
+  document.querySelector( "#main > footer > div > div > div > div.copyable-text" ).dispatchEvent( evt );
+  document.querySelector( "#main > footer > div > div > button > span" ).click( );
+  
+} 
+
+
+
+/////////////////////////   Get the latest message   /////////////////////////
+
+// GetLatestMessage gets the last message that you've received and returns it as a string
+function GetLatestMessage( ){
+	
+	//return document.querySelector( `#main > div > div > div > div > div.message-out:nth-last-of-type(1) > div > div > div > div > div > span > span` ).innerHTML;
+  return document.querySelector( `#main > div > div > div > div > div:nth-last-of-type(1) > div > div > div > div > div > span > span` ).innerHTML;
+  
+}
+
+
+
+/////////////////////////   Check if a new message is send   /////////////////////////
+
+// sOldText contains the innerHTML of the loaded messages as a string
+let sOldText = document.querySelector( "#main > div > div > div" ).innerHTML;
+
+// CheckForNewMessage checks if the old loaded messages are the same as the new ones
+// and if not, it will process the new message
+setInterval(
+
+  function /*CheckForNewMessage*/( ){
+	
+    //get the new text
+    let sNewText = document.querySelector( "#main > div > div > div" ).innerHTML;
+  
+    //if they aren't the same, process the new message
+    if( sOldText != sNewText ){
+	  
+	  sOldText = sNewText;
+	  ProcessMessage( GetLatestMessage( ) );
+	
+    }
+  
+  }, 500
+
+);
+
+
+
+/////////////////////////   Main function   /////////////////////////
+
+function Main( ){
+  
+  let chatName = document.querySelector( "#main > header > div[role='button'] > div > div > span[dir='auto']" ).innerHTML;
+  
+  Send( `The bot 'botje' is set up and bound to the chat: "${chatName}".\nUse !help to see the commands you can use.\nCreated by Roel` );
+  
+}
+
+Main( );
+
+
+
+
